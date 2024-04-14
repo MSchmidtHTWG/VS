3297d8ccb59aafa34578202464a8dad69f92708c
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2024-04-15 01:55:59 +0200

    Decompiling .class

---
+++ messaging/Message.java
@@ -0,0 +1,25 @@
+package messaging;
+
+    // Source code is decompiled from a .class file using FernFlower decompiler.
+
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+
+public class Message {
+   private final Serializable payload;
+   private final InetSocketAddress sender;
+
+   public Message(Serializable payload, InetSocketAddress sender) {
+      this.payload = payload;
+      this.sender = sender;
+   }
+
+   public Serializable getPayload() {
+      return this.payload;
+   }
+
+   public InetSocketAddress getSender() {
+      return this.sender;
+   }
+}
+
